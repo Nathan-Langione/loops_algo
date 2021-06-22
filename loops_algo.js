899379a6e000f46b5db043a3ef20968e0e8fe944
Nathan Langione <nlangione@wayfair.com>
2021-06-22 13:29:50 -0400

    Adding assignment

---
+++ loops_algo.js
@@ -0,0 +1,14 @@
+console.log("Print odds 1-20")
+
+for (i = 0; i <= 20; i++){
+    if (i%2!=0)
+    console.log(i)
+}
+
+console.log("Sum and Print 1-5")
+var sum = 0
+for (i = 1; i <= 5; i++){
+    sum+=i;
+    console.log("Num:"+i+", Sum:"+sum)
+    console.log
+}
